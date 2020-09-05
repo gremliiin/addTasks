cda17d30399fa6bec1278b442ca132bd7195d06f
gremliiin <rekufo@gmail.com>
2020-09-05 18:25:20 +0600

    Updates

---
+++ precache-manifest.75247bdc9b45836a3edb436c99f44d81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "433d4b1e55176dbe2821d9b9df072bdc",
+    "url": "/addTasks/index.html"
+  },
+  {
+    "revision": "e10260ca2320687db130",
+    "url": "/addTasks/static/css/main.a55d0395.chunk.css"
+  },
+  {
+    "revision": "27e9b4603cce9df10c45",
+    "url": "/addTasks/static/js/2.7984ee40.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/addTasks/static/js/2.7984ee40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e10260ca2320687db130",
+    "url": "/addTasks/static/js/main.0862a908.chunk.js"
+  },
+  {
+    "revision": "046fddc320981da897c5",
+    "url": "/addTasks/static/js/runtime-main.4bc41989.js"
+  }
+]);
